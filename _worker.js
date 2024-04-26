588be27a928b8939ff88cf43ca7c4e7f3427e090
dj56959566 <167070575+dj56959566@users.noreply.github.com>
2024-04-26 13:10:14 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,18 +1,89 @@
 
 // 部署完成后在网址后面输入/，加上下方设置的mytoken=的值，即可获取聚合后的节点
 
-let mytoken = 'auto'; //可根据需要自行设定，或者使用UUID在线生成网站进行生成：https://1024tools.com/uuid
+let mytoken = 'auto1'; //可根据需要自行设定，或者使用UUID在线生成网站进行生成：https://1024tools.com/uuid
 let BotToken =''; //可以为空，或电报@BotFather关注机器人，输入/start，选择：/newbot进行获取
 let ChatID =''; //可以为空，或电报@userinfobot中获取，选择：/start进行获取
 let TG = 0; //新手勿动， 开发者选项，1 为推送所有的访问信息，0 为不推送订阅转换后端的访问信息与异常访问
 let FileName = 'Pages-SUB-Convert';
-let SUBUpdateTime = 6; //更改此次设置订阅更新时间，单位小时
+let SUBUpdateTime = 3; //更改此次设置订阅更新时间，单位小时
 
 //在以下输入您的自建节点链接或订阅链接，请保留双引号
 let MainData = `
-vless://b7a392e2-4ef0-4496-90bc-1c37bb234904@cf.090227.xyz:443?encryption=none&security=tls&sni=edgetunnel-2z2.pages.dev&fp=random&type=ws&host=edgetunnel-2z2.pages.dev&path=%2F%3Fed%3D2048#%E5%8A%A0%E5%85%A5%E6%88%91%E7%9A%84%E9%A2%91%E9%81%93https%3A%2F%2Ft.me%2FOneZyhCN%E8%8E%B7%E5%8F%96%E6%9B%B4%E5%A4%9A%E8%B5%84%E6%BA%90
-https://sub.xf.free.hr/auto
-https://hy2sub.pages.dev
+vless://b59d4e4a-5efe-4629-8358-208d6b0f5666@104.18.105.41:443?encryption=none&security=tls&sni=pages-3jp.pages.dev&fp=randomized&type=ws&host=pages-3jp.pages.dev&path=%2F%3Fed%3D2048#pages-3jp.pages.dev
+vless://b59d4e4a-5efe-4629-8358-208d6b0f5666@104.17.231.34:443?encryption=none&security=tls&sni=pages-3jp.pages.dev&fp=randomized&type=ws&host=pages-3jp.pages.dev&path=%2F%3Fed%3D2048#pages-3jp.pages.dev
+vless://b59d4e4a-5efe-4629-8358-208d6b0f5666@cip.951535.xyz:443?encryption=none&security=tls&sni=pages-3jp.pages.dev&fp=randomized&type=ws&host=pages-3jp.pages.dev&path=%2F%3Fed%3D2048#pages-3jp.pages.dev
+vless://b59d4e4a-5efe-4629-8358-208d6b0f5666@[2606:4700:d0::285e:a09d:9e2f:d7db]:3476?encryption=none&security=tls&sni=pages-3jp.pages.dev&fp=randomized&type=tcp&headerType=none&host=2606%3A4700%3Ad0%3A%3A285e%3Aa09d%3A9e2f%3Ad7db#pages-3jp.pages.dev
+vless://b59d4e4a-5efe-4629-8358-208d6b0f5666@104.16.169.130:443?encryption=none&security=tls&sni=pages-3jp.pages.dev&fp=randomized&type=ws&host=pages-3jp.pages.dev&path=%2F%3Fed%3D2048#pages-3jp.pages.dev
+vless://b59d4e4a-5efe-4629-8358-208d6b0f5666@104.16.190.116:443?encryption=none&security=tls&sni=pages-3jp.pages.dev&fp=randomized&type=ws&host=pages-3jp.pages.dev&path=%2F%3Fed%3D2048#pages-3jp.pages.dev
+vless://b59d4e4a-5efe-4629-8358-208d6b0f5666@104.17.15.77:443?encryption=none&security=tls&sni=pages-3jp.pages.dev&fp=randomized&type=ws&host=pages-3jp.pages.dev&path=%2F%3Fed%3D2048#pages-3jp.pages.dev
+vless://b59d4e4a-5efe-4629-8358-208d6b0f5666@104.17.143.181:443?encryption=none&security=tls&sni=pages-3jp.pages.dev&fp=randomized&type=ws&host=pages-3jp.pages.dev&path=%2F%3Fed%3D2048#pages-3jp.pages.dev
+vless://b59d4e4a-5efe-4629-8358-208d6b0f5666@104.17.250.163:443?encryption=none&security=tls&sni=pages-3jp.pages.dev&fp=randomized&type=ws&host=pages-3jp.pages.dev&path=%2F%3Fed%3D2048#pages-3jp.pages.dev
+vless://b59d4e4a-5efe-4629-8358-208d6b0f5666@104.16.229.135:443?encryption=none&security=tls&sni=pages-3jp.pages.dev&fp=randomized&type=ws&host=pages-3jp.pages.dev&path=%2F%3Fed%3D2048#pages-3jp.pages.dev
+vless://b59d4e4a-5efe-4629-8358-208d6b0f5666@47.74.37.227:80?encryption=none&security=tls&sni=pages-3jp.pages.dev&fp=randomized&type=ws&host=pages-3jp.pages.dev&path=%2F%3Fed%3D2048#pages-3jp.pages.dev
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@47.91.17.102:2083?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.jp.fxxk.dedyn.io#JP
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@221.139.226.21:18245?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.kr.fxxk.dedyn.io#KR
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@47.74.11.251:2053?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.jp.fxxk.dedyn.io#JP
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@104.18.82.137:443?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.oracle.fxxk.dedyn.io#CMCC
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@193.9.46.16:8443?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.us.fxxk.dedyn.io#US
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@[2606:4700:e7:2e65:613f:1bc0:32f9:653f]:443?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.oracle.fxxk.dedyn.io#CMCC-IPV6
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@38.47.96.138:14217?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.jp.fxxk.dedyn.io#JP
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@[2606:4700:e7::8ef6:20b6:f618]:443?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.oracle.fxxk.dedyn.io#CMCC-IPV6
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@[2606:4700:e7:be:3050:3ac4:ad67:aa45]:443?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.oracle.fxxk.dedyn.io#CMCC-IPV6
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@cf.090227.xyz:443?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.us.fxxk.dedyn.io#%E5%8A%A0%E5%85%A5%E6%88%91%E7%9A%84%E9%A2%91%E9%81%93t.me%2FCMLiussss%E8%A7%A3%E9%94%81%E6%9B%B4%E5%A4%9A%E4%BC%98%E9%80%89%E8%8A%82%E7%82%B9
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@104.16.199.141:443?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.oracle.fxxk.dedyn.io#CMCC
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@[2606:4700:e7:0:f1:1382:73b5:996a]:443?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.oracle.fxxk.dedyn.io#CMCC-IPV6
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@104.17.153.202:443?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.oracle.fxxk.dedyn.io#CMCC
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@[2606:4700:e7:be:3050:3a3c:28f9:bfd3]:443?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.oracle.fxxk.dedyn.io#CMCC-IPV6
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@148.135.98.86:12526?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.us.fxxk.dedyn.io#US
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@47.74.6.16:2087?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.jp.fxxk.dedyn.io#JP
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@47.74.9.174:2087?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.jp.fxxk.dedyn.io#JP
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@47.74.43.24:8443?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.jp.fxxk.dedyn.io#JP
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@129.153.213.8:443?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.us.fxxk.dedyn.io#US
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@158.178.240.232:23559?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.sg.fxxk.dedyn.io#SG
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@[2606:4700:3036:bc:ea06:32a9:f4a4:5c12]:443?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.oracle.fxxk.dedyn.io#CMCC-IPV6
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@104.16.38.173:443?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.oracle.fxxk.dedyn.io#CMCC
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@134.195.211.16:4433?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.us.fxxk.dedyn.io#US
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@222.108.215.96:11180?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.kr.fxxk.dedyn.io#KR
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@104.17.52.252:443?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.oracle.fxxk.dedyn.io#CMCC
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@129.146.46.164:443?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.us.fxxk.dedyn.io#US
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@[2606:4700:3036::1a:733d:8fd3]:443?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.oracle.fxxk.dedyn.io#CMCC-IPV6
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@47.242.171.94:2053?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.aliyun.fxxk.dedyn.io#HK
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@47.242.171.94:2087?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.aliyun.fxxk.dedyn.io#HK
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@47.242.234.127:2096?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.aliyun.fxxk.dedyn.io#HK
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@47.242.171.94:8443?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.aliyun.fxxk.dedyn.io#HK
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@43.128.6.137:10001?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.aliyun.fxxk.dedyn.io#HK
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@47.242.234.127:2053?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.aliyun.fxxk.dedyn.io#HK
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@8.218.156.91:2053?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.aliyun.fxxk.dedyn.io#HK
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@47.242.234.127:2083?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.aliyun.fxxk.dedyn.io#HK
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@47.245.8.68:2096?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.jp.fxxk.dedyn.io#JP
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@141.144.207.78:443?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.oracle.fxxk.dedyn.io#NL
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@158.180.230.155:2053?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.oracle.fxxk.dedyn.io#IT
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@152.70.234.249:21847?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.kr.fxxk.dedyn.io#KR
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@144.202.92.104:31000?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.us.fxxk.dedyn.io#US
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@192.9.246.112:8443?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.us.fxxk.dedyn.io#US
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@34.123.83.237:443?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.us.fxxk.dedyn.io#US
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@8.210.131.161:34237?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.aliyun.fxxk.dedyn.io#HK
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@108.165.169.221:8443?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.us.fxxk.dedyn.io#US
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@47.91.3.179:2087?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.jp.fxxk.dedyn.io#JP
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@158.178.241.45:2096?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.sg.fxxk.dedyn.io#SG
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@129.150.63.96:2087?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.sg.fxxk.dedyn.io#SG
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@51.107.6.231:8443?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.oracle.fxxk.dedyn.io#CH
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@129.150.63.96:2053?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.sg.fxxk.dedyn.io#SG
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@129.150.58.86:57621?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.sg.fxxk.dedyn.io#SG
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@47.245.41.84:2096?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.jp.fxxk.dedyn.io#JP
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@51.107.6.220:8443?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.oracle.fxxk.dedyn.io#CH
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@168.138.185.28:2053?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.sg.fxxk.dedyn.io#SG
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@47.243.70.197:2053?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.aliyun.fxxk.dedyn.io#HK
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@131.186.28.236:443?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.kr.fxxk.dedyn.io#KR
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@193.123.250.209:2087?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.kr.fxxk.dedyn.io#KR
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@[2606:4700:3036:0:b14e:3fa2:b8a7:dc25]:443?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.oracle.fxxk.dedyn.io#CMCC-IPV6
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@211.186.222.122:10250?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.kr.fxxk.dedyn.io#KR
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@193.111.31.133:50896?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.jp.fxxk.dedyn.io#JP
+vless://f3862bd7-0fd0-4276-81c2-e705aa8077b3@103.99.179.88:1001?encryption=none&security=tls&sni=one.56v03.dynv6.net&fp=random&type=ws&host=one.56v03.dynv6.net&path=%2FproxyIP%3Dproxyip.us.fxxk.dedyn.io#US
+
+https://ghproxy.com/https://raw.githubusercontent.com/a2470982985/getNode/main/v2ray.txt
+
 `
 
 //请将机场订阅链接填入上方
